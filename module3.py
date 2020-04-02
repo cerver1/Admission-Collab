993fd163fc0aa84cde0f833d16cb416d8c1bbec0
deepanshu <deepanshuchg@gmail.com>
2020-04-02 15:25:41 +0530

    Updated the 2nd and 3rd module

--- module3.py
+++ module3.py
@@ -1,11 +1,13 @@
 from module1 import studentStatus
+from module2 import module2score
 
 #missing process: combining the points from module 1 and module 2 
-
-if studentStatus() > 50 :
+totalPoints = studentStatus() + module2score()
+if totalPoints> 100 :
     
     print(f"\nThis Student has been admitted!\n")
 
 else:
 
     print(f"\nUnfortunately this Student has been denied due to scores falling below requirements.\n")
+    print(totalPoints)
